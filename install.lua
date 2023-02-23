84cc12a15035fe114ce9b6e48b93e9b4968a64a0
Codermsk <62512817+Codermsk@users.noreply.github.com>
2023-02-23 17:50:30 +0300

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,4 +1,4 @@
-local REPOSITOTY = "https://raw.githubusercontent.com/Codermsk/slot_mc/master"
+local REP = "https://raw.githubusercontent.com/Codermsk/slot_mc/master"
 
 local shell = require("shell")
 shell.execute("wget -fq " .. REP .. "/launcher.lua /home/1.lua")
